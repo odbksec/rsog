f50947cdcb0c09e3e3f9eb9a2ec8d058d841dfee
odbksec <79425414+odbksec@users.noreply.github.com>
2021-02-22 23:40:51 +0100

    tiene error en el check port

--- rsog.sh
+++ rsog.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 #
 #
 #	OneLiner Reverse Shell
 #
-#					ODBK
+#					ODBK   |   odbksec@protonmail.com
+#
 
-source colores
+source network
+source plantillas
 source oneliner
 
 export DEBIAN_FRONTEND=noninteractive
@@ -14,69 +16,75 @@ export DEBIAN_FRONTEND=noninteractive
 trap ctrl_c INT
 
 function ctrl_c(){
-	echo -e "\n${amarillo} - Salida Forzada ${finColor}"
+	error "Forzando salida"
 	exit 0
 }
 
 function ayuda(){
 	# Cargo los oneliners
-	shells=`cut -d'=' -f1 oneliner`
-	langs=`echo $shells`
-	echo -e "\n${verde}Reverse Shell Oneliner Generator${finColor}"
-	echo -e "\t ${azul}-i${finColor} Interfaz de la cual se obtendrá la IP"
-        echo -e "\t ${azul}-p${finColor} Puerto a la escucha. Defecto: 4444"
-        echo -e "\t ${azul}-l${finColor} Lenguaje usado para generar la shell. Disponibles:"
-	echo -e "\t\t${amarillo}${langs}${finColor}"
-        echo -e "\t ${azul}-h${finColor} Muestra esta ayuda"
+	informacion "${azul}-i${finColor} Interfaz de la cual se obtendrá la IP"
+    informacion "${azul}-l${finColor} IP Local (opcional)"
+    informacion "${azul}-p${finColor} Puerto a la escucha. Defecto: 4444"
+    informacion "${azul}-s${finColor} Script a usar para generar la shell. Disponibles:"
+	informacion "\t${amarillo}${langs}${finColor}"
+    informacion "${azul}-h${finColor} Muestra esta ayuda"
+    echo -e "\n"
 	exit 0
 }
 
 
 #	MAIN
+	banner "${verde}Reverse Shell Oneliner Generator${finColor}"
+	shells=`cut -d'=' -f1 oneliner`
+	langs=`echo $shells`
 
-	while getopts ":i:p:l:h" arg; do
+	while getopts ":i:l:p:s:h" arg; do
 		case $arg in
 			i) iface=$OPTARG;;
 			p) port=$OPTARG;;
-			l) lang=$OPTARG;;
+			s) lang=$OPTARG;;
+			l) lhost=$OPTARG;;
 			h) ayuda;;
 		esac
 	done
 
-	echo -e "\n${verde}Reverse Shell Oneliner Generator${finColor}"
-
-	shells=`cut -d'=' -f1 oneliner`
 
 	# Checamos el puerto
-	if [[ $port -eq $port && $port -lt 65536 ]]; then
-                echo -e "\n\t${verde}Puerto Seleccionado: ${finColor}"$port
-        else
-                port=4444
-        fi
+	if  [ $(checkPort $port) ] ; then
+        informacion "${verde}Puerto Local: ${finColor}"$port
+    else
+    	port=4444
+    fi
 
+    if [[ $lhost ]]; then
+    	ip=$lhost
+        informacion "${verde}IP local: ${finColor}"$lhost
+    else
+    	# Cargamos todas las interfaces
+		ifaces=($(ifconfig | grep flags | awk '{ print $1}' | sed -e 's/:$//'))
 
-	# Cargamos todas las interfaces
-	ifaces=($(ifconfig | grep flags | awk '{ print $1}' | sed -e 's/:$//'))
-
-	# Checamos que  la interfaz elegida exista si no fallo
-	if [[ "${ifaces[@]}" =~ "${iface}" ]]; then
-		ip=`ifconfig ${iface} | grep "inet" | grep -v "inet6" | awk '{ print $2}'`
-		echo -e "\t${verde}IP Seleccionada: ${finColor}"$ip
-	else
-		echo -e "\n${rojo}Interfaz erronea .... Saliendo${finColor}"
-		exit 0
+		# Checamos que  la interfaz elegida exista si no fallo
+		if [[ "${ifaces[@]}" =~ "${iface}" ]]; then
+			ip=`ifconfig ${iface} | grep "inet" | grep -v "inet6" | awk '{ print $2}'`
+			informacion "${verde}IP Local: ${finColor}"$ip
+		else
+			informacion "${rojo}Interfaz erronea .... Saliendo${finColor}"
+			exit 0
+		fi
 	fi
+	
 
 	# Checamos lenguaje
 	if [[ "${shells[@]}" =~ "${lang}" ]]; then
 		line=`eval echo '$'$lang`
-		echo -e "\t${verde}RShell Seccionada: ${finColor}"$line
-        	oneliner=${line/"##ip##"/$ip}
-        	oneliner=${oneliner/"##port##"/$port}
-                echo $oneliner | xclip -sel clip
-                echo -e "\n${amarillo}Resultado: ${finColor}"$oneliner"\n\n${violeta}En su clipboard si tiene instalado XCLIP"$finColor
-        else
-                echo -e "\n${rojo}Lenguaje no disponible .... Saliendo${finColor}"
-                exit 0
-        fi
+		informacion "${verde}Payload: ${finColor}"$line
+      	oneliner=${line/"##ip##"/$ip}
+       	oneliner=${oneliner/"##port##"/$port}
+        echo $oneliner | xclip -sel clip
+        resultado "${amarillo}Resultado\n ${finColor}"$oneliner
+        informacion "${turquesa}En su clipboard si tiene instalado XCLIP"$finColor
+    else
+        error "Lenguaje no disponible .... Saliendo"
+        exit 0
+    fi
 
